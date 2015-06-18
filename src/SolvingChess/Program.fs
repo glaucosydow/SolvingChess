5ae5cf2b7ecbfb5ac903b399b2f8c7b32278965c
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2015-06-18 15:25:43 -0300

    working with puzzle #6

--- src/SolvingChess/Program.fs
+++ src/SolvingChess/Program.fs
@@ -6,22 +6,21 @@ open Moves
 open MateFinder
 
 let startpos = { 
-    EmptyBoard with 
-                WhiteKing = G1;
-                WhiteQueens = H5;
-                WhitePawns = A2 ||| B2 ||| C2 ||| G2 ||| H2;
-                WhiteBishops = C4 ||| E3;
-                WhiteKnights = D2 ||| D4;
-                WhiteRooks = A1 ||| F4;
+        EmptyBoard with 
+                    WhiteKing = G1;
+                    WhiteQueens = C2;
+                    WhitePawns = B3 ||| F2 ||| G3 ||| H2;
+                    WhiteBishops = B2 ||| G2;
+                    WhiteRooks = C1 ||| D1;
                 
-                BlackKing = G7;
-                BlackQueens = D8;
-                BlackPawns = A7 ||| B7 ||| E5 ||| G6 ||| H7;
-                BlackBishops = F8 ||| C8;
-                BlackKnights = B8 ||| F6;
-                BlackRooks = A8 ||| H8;
+                    BlackKing = F7;
+                    BlackQueens = D8
+                    BlackPawns = B7 ||| D5 ||| G7 ||| H6;
+                    BlackBishops = E7 ||| C8;
+                    BlackKnights = D7;
+                    BlackRooks = A8 ||| E8;
 
-                SideToMove = White
+                    SideToMove = White
 }
 
 
@@ -33,13 +32,18 @@ let printmoves history =
 
 open Check
 open Attacks
+open System.Diagnostics
 [<EntryPoint>]
 let main argv = 
     //moves startpos |> Seq.iter(fun move -> printfn "%s" (move.ToString()))
+    let sw = Stopwatch()
+    sw.Start()
+
     let mateline = (findMate startpos 0 8)
     match mateline with 
     | Some(line) -> printmoves line
     | None -> printfn "There is no answer"
 
-    printfn "number of calls: %d" numberOfCalls
+    printfn "ply count: %d" numberOfCalls
+    printfn "time (ms): %d" (sw.ElapsedMilliseconds)
     0
