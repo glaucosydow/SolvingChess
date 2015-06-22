1a1eaf48e1934a108f11b092c05b452d955aa270
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2015-06-22 13:33:07 -0300

    check status verification optimization

--- src/SolvingChess/Attacks.fs
+++ src/SolvingChess/Attacks.fs
@@ -5,9 +5,11 @@ open Position
 open BoardShifter
 open BitOperations
 
-let private ka =    Array.zeroCreate<Bitboard> 64
-let private karea = Array.zeroCreate<Bitboard> 64
+let ka =    Array.zeroCreate<Bitboard> 64
+let karea = Array.zeroCreate<Bitboard> 64
 let knightAttacksPC =   Array.zeroCreate<Bitboard> 64
+let whitePawnAttackersPC = Array.zeroCreate<Bitboard> 64
+let blackPawnAttackersPC = Array.zeroCreate<Bitboard> 64
 
 for i = 0 to 63 do
     let s = sqFromIndex i
@@ -21,11 +23,19 @@ for i = 0 to 63 do
     knightAttacksPC.[i] <- cs 1 2 s ||| cs  1 -2 s ||| cs  2 1 s||| cs  2 -1 s||| cs -1 2 s |||
                cs -1 -2 s ||| cs -2 1 s ||| cs -2 -1 s
 
+    whitePawnAttackersPC.[i] <- cs -1 -1 s ||| cs -1 1 s
+    blackPawnAttackersPC.[i] <- cs 1 -1 s ||| cs 1 1 s
 
-let kingAttacks (kingPosition : Bitboard) = 
+let inline whitePawnAttackers sq =
+    whitePawnAttackersPC.[lsb sq]
+
+let inline blackPawnAttackers sq =
+    blackPawnAttackersPC.[lsb sq]
+
+let inline kingAttacks (kingPosition : Bitboard) = 
     ka.[lsb kingPosition]
 
-let kingArea (kingPosition: Bitboard) =
+let inline kingArea (kingPosition: Bitboard) =
     karea.[lsb kingPosition]
 
 let whitePawnsAttacks (whitePawnsPositions : Bitboard) =
@@ -33,7 +43,6 @@ let whitePawnsAttacks (whitePawnsPositions : Bitboard) =
 
 let blackPawnsAttacks (blackPawnsPositions : Bitboard) =
     (chessShift -1 -1 blackPawnsPositions) ||| (chessShift -1 1 blackPawnsPositions)
-
 
 let inline knightAttacks sq = 
     knightAttacksPC.[lsb sq]
@@ -61,7 +70,7 @@ let rooksAttacks rooksPositions friends enemies =
     |> Seq.map(fun(sq) -> rookAttacks sq allpieces)
     |> Seq.fold (fun acc elem -> acc ||| elem) 0UL
 
-let bishopAttacks sq allpieces =
+let inline bishopAttacks sq allpieces =
     let a = except sq (diagonalNWOfSquare sq ||| diagonalNEOfSquare sq)
     let b = except (rayToNEFromSquare (((rayToNEFromSquare sq) &&& allpieces).lsb() |> sqFromIndex)) a
     let c = except (rayToNWFromSquare (((rayToNWFromSquare sq) &&& allpieces).lsb() |> sqFromIndex)) b
@@ -74,7 +83,7 @@ let bishopsAttacks bishopsPositions friends enemies =
     |> Seq.map(fun(sq) -> bishopAttacks sq allpieces)
     |> Seq.fold (fun acc elem -> acc ||| elem) 0UL
 
-let queenAttacks sq allpieces = 
+let inline queenAttacks sq allpieces = 
     (bishopAttacks sq allpieces) ||| (rookAttacks sq allpieces)
 
 let queensAttacks queensPositions friends enemies = 
--- src/SolvingChess/Check.fs
+++ src/SolvingChess/Check.fs
@@ -1,13 +1,51 @@
 ﻿module Check
 
 open BitOperations
+open BoardUnits
 open Position
 open Attacks
 
+let isBlackKingUnderAttack (position: Position) =
+    let allpieces = position.AllPieces
+
+    let condition = 
+        ((knightAttacks         position.BlackKing) &&& position.WhiteKnights) <> 0UL ||
+        ((whitePawnAttackers    position.BlackKing) &&& position.WhitePawns) <> 0UL ||
+        ((kingAttacks           position.BlackKing) &&& position.WhiteKing) <> 0UL 
+
+    if condition 
+    then true 
+    else 
+        let ra = rookAttacks position.BlackKing allpieces
+        if ((ra &&& position.WhiteRooks) <> 0UL || (ra &&& position.WhiteQueens) <> 0UL)
+        then true
+        else
+            let ba = bishopAttacks position.BlackKing allpieces
+            ((ba &&& position.WhiteBishops) <> 0UL || (ba &&& position.WhiteQueens) <> 0UL)
+
+let isWhiteKingUnderAttack (position: Position) =
+    let allpieces = position.AllPieces
+
+    let condition = 
+        ((knightAttacks      position.WhiteKing) &&& position.BlackKnights) <> 0UL ||
+        ((blackPawnAttackers position.WhiteKing) &&& position.BlackPawns) <> 0UL ||
+        ((kingAttacks        position.WhiteKing) &&& position.BlackKing) <> 0UL 
+
+    if condition 
+    then true 
+    else 
+        let ra = rookAttacks position.WhiteKing allpieces
+        if ((ra &&& position.BlackRooks) <> 0UL || (ra &&& position.BlackQueens) <> 0UL)
+        then true
+        else
+            let ba = bishopAttacks position.WhiteKing allpieces
+            ((ba &&& position.BlackBishops) <> 0UL || (ba &&& position.BlackQueens) <> 0UL)
+
+
 let isKingUnderAttack side position =
     match side with 
-    | White -> position.WhiteKing <> 0UL && (isSet position.WhiteKing (blackAttacks position false))
-    | Black -> position.BlackKing <> 0UL && (isSet position.BlackKing (whiteAttacks position false))
+    | White -> position.WhiteKing <> 0UL && isWhiteKingUnderAttack position
+    | Black -> position.BlackKing <> 0UL && isBlackKingUnderAttack position
 
 let isCheck position = 
     isKingUnderAttack position.SideToMove position
